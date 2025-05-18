d56d016326f3956e24b6747d6334b9252cee97b9
soyeon <ryumirr1@naver.com>
2025-05-19 01:21:26 +0900

    feature/add-test-folders

---
+++ api/inventory-api/src/test/java/api/inventory/InventoryApiTest.java
@@ -0,0 +1,13 @@
+package api.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+class InventoryApiTest {
+
+	@Test
+	void InventoryApiTest_test() {
+	}
+}
