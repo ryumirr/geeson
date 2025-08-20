15dba70315a4ca08fc35676932ed77c7b04aac88
soyeon <ryumirr1@naver.com>
2025-08-21 08:33:26 +0900

    fix:ai 커멘트 수정하다

--- infra/grpc-client/src/main/java/grpc/client/ShipmentGrpcClient.java
+++ infra/grpc-client/src/main/java/grpc/client/ShipmentGrpcClient.java
@@ -8,12 +8,12 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-import net.devh.boot.grpc.client.inject.GrpcClient;
+// import net.devh.boot.grpc.client.inject.GrpcClient;
 
 @Service
 public class ShipmentGrpcClient {
 
-    @GrpcClient("shipment")
+    // @GrpcClient("shipment")
     private ShipmentServiceBlockingStub shipmentStub;
 
     @PostConstruct
